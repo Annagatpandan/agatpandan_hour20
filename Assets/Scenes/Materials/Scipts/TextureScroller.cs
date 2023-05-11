36fc3b5e55202cf216ded29ceea3877d0631dd1f
Annagatpandan <kissyndelilah6012@gmail.com>
2023-05-11 12:22:41 -0700

    Fixing scripting errors

--- Assets/Scenes/Materials/Scipts/TextureScroller.cs
+++ Assets/Scenes/Materials/Scipts/TextureScroller.cs
@@ -1,13 +1,12 @@
 using System.Collections;
-using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+[DebuggerDisplay( { nameof ("GetDebuggerDisplay , nq {)} ")] 
+public class TextureScroller : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        
+    
     }
 
     // Update is called once per frame
@@ -15,4 +14,4 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
-}
+
