9baa54a73a19b8e3623ea6fb2a83acbc9313b6e8
Annagatpandan <kissyndelilah6012@gmail.com>
2023-05-11 18:49:54 -0700

    Added a semicolon after the ) in line 8

--- Assets/Scenes/Materials/Scipts/TextureScroller.cs
+++ Assets/Scenes/Materials/Scipts/TextureScroller.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Diagnostics;
 using UnityEngine;
-[DebuggerDisplay { nameof ("(GetDebuggerDisplay , nq {)} ")] };
+[DebuggerDisplay { nameof (("GetDebuggerDisplay , nq {)} ")] };
 public class TextureScroller : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Start();
     
     }
 
