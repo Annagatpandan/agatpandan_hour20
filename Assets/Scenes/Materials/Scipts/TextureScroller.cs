e1b247bdbaed3ec30bb91df3e19f7cd0a6d84a89
Annagatpandan <kissyndelilah6012@gmail.com>
2023-05-25 17:40:15 -0700

    Added a troll prefab to the scene

--- Assets/Scenes/Materials/Scipts/TextureScroller.cs
+++ Assets/Scenes/Materials/Scipts/TextureScroller.cs
@@ -1,17 +1,35 @@
-using System.Collections;
+
 using System.Diagnostics;
 using UnityEngine;
-[DebuggerDisplay { nameof (("GetDebuggerDisplay , nq {)} ")] };
-public class TextureScroller : MonoBehaviour
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+
+public class TextureScroller : MonoBehaviour ; 
+ 
+ string "GetDebuggerDisplay"();
 {
-    // Start is called before the first frame update
-    void Start();
-    
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
+}
+    // Start is called before the first frame update
+public float speed = .5f;
+    private float speed = .5f;
+
+renderer renderer;
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+    // Increase offset based on time
+    offset += Time.deltaTime * speed;
+    // Keep offset between 0 and 1
+    if (offset > 1)
+        offset -= 1;
+    // Apply the offset to the material
+    renderer.material.mainTextureOffset = new Vector2(0, offset);
+}
 
